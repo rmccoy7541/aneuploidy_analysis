9ca9c995b53950e074de11d085a718035fc68a51
Rajiv McCoy <rcmccoy@uw.edu>
2015-08-31 13:49:53 -0700

    Update f09_pBPH_by_paternal_age.R

--- figures/f09_pBPH_by_paternal_age.R
+++ figures/f09_pBPH_by_paternal_age.R
@@ -71,7 +71,7 @@ results_te$X6 <- "Day-5 TE"
 
 results_all_chroms <- rbind(results_blastomere, results_te)
 
-c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Paternal Age") + ylab("Prop. with Paternal BPH Aneuploidy") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
+c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Paternal Age") + ylab("Prop. with Paternal BPH Error") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
 
 c <- c + facet_grid(. ~ X6)
 
@@ -81,10 +81,10 @@ meiotic_blastomere <- meioticByCase(data_blastomere)
 meiotic_te <- meioticByCase(data_te)
 
 paternal_age <- data.frame(aggregate(data$paternal_age ~ data$case, FUN = mean))
-names(paternal_age) <- c("i", "paternal_age")
-meiotic_blastomere <- merge(meiotic_blastomere, paternal_age, "i")
-meiotic_te <- merge(meiotic_te, paternal_age, "i")
+names(paternal_age) <- c("case", "paternal_age")
+meiotic_blastomere <- merge(meiotic_blastomere, paternal_age, "case")
+meiotic_te <- merge(meiotic_te, paternal_age, "case")
 
 
-summary(glm(data = meiotic_blastomere, formula = cbind(euploid, meiotic) ~ paternal_age, family = quasibinomial(link = "logit")))
-summary(glm(data = meiotic_te, formula = cbind(euploid, meiotic) ~ paternal_age, family = quasibinomial(link = "logit")))
+summary(glm(data = meiotic_blastomere, formula = cbind(euploid, aneuploid) ~ paternal_age, family = quasibinomial(link = "logit")))
+summary(glm(data = meiotic_te, formula = cbind(euploid, aneuploid) ~ paternal_age, family = quasibinomial(link = "logit")))
