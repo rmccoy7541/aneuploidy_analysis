ce3b82609d8de2d14168deffa85b202d9190654c
Rajiv McCoy <rcmccoy@uw.edu>
2015-09-02 10:16:52 -0700

    Update table_2.R

--- table_2.R
+++ table_2.R
@@ -3,7 +3,7 @@ library(gridExtra)
 library(RCurl)
 library(data.table)
 
-source('~/Desktop/aneuploidy-analysis/aneuploidy_functions.R', chdir = TRUE)
+source('~/Desktop/aneuploidy_analysis-master/aneuploidy_functions.R', chdir = TRUE)
 
 URL <- "https://raw.githubusercontent.com/rmccoy7541/aneuploidy-analysis/master/data/aaa3337-McCoy-SM.table_S2.csv" # import the data
 url <- getURL(URL)
@@ -49,6 +49,55 @@ totalChroms <- function(data) {
 	return(rowSums(chroms_frame, na.rm = T))
 }
 
+totalMatChroms <- function(data) {
+	chroms_frame <- data.frame(matrix(ncol = 23, nrow = nrow(data)))
+	aneuploid_frame <- data[, 7:29, with = F]
+	chroms_frame[aneuploid_frame == "H110"] <- 1
+	chroms_frame[aneuploid_frame == "H101"] <- 1
+	chroms_frame[aneuploid_frame == "H011"] <- 0
+	chroms_frame[aneuploid_frame == "H210"] <- 2
+	chroms_frame[aneuploid_frame == "H120"] <- 1
+	chroms_frame[aneuploid_frame == "H111"] <- 1
+	chroms_frame[aneuploid_frame == "H201"] <- 2
+	chroms_frame[aneuploid_frame == "H102"] <- 1
+	chroms_frame[aneuploid_frame == "H100"] <- 1
+	chroms_frame[aneuploid_frame == "H010"] <- 0
+	chroms_frame[aneuploid_frame == "H001"] <- 0
+	chroms_frame[aneuploid_frame == "H000"] <- 0
+	return(rowSums(chroms_frame, na.rm = T))
+}
+
+totalPatChroms <- function(data) {
+	chroms_frame <- data.frame(matrix(ncol = 23, nrow = nrow(data)))
+	aneuploid_frame <- data[, 7:29, with = F]
+	chroms_frame[aneuploid_frame == "H110"] <- 1
+	chroms_frame[aneuploid_frame == "H101"] <- 1
+	chroms_frame[aneuploid_frame == "H011"] <- 2
+	chroms_frame[aneuploid_frame == "H210"] <- 1
+	chroms_frame[aneuploid_frame == "H120"] <- 2
+	chroms_frame[aneuploid_frame == "H111"] <- 2
+	chroms_frame[aneuploid_frame == "H201"] <- 1
+	chroms_frame[aneuploid_frame == "H102"] <- 2
+	chroms_frame[aneuploid_frame == "H100"] <- 0
+	chroms_frame[aneuploid_frame == "H010"] <- 1
+	chroms_frame[aneuploid_frame == "H001"] <- 1
+	chroms_frame[aneuploid_frame == "H000"] <- 0
+	return(rowSums(chroms_frame, na.rm = T))
+}
+
+set.seed(42)
+data_blastomere$maternalChroms <- totalMatChroms(data_blastomere)
+data_blastomere$paternalChroms <- totalPatChroms(data_blastomere)
+data_blastomere$totalChroms <- totalChroms(data_blastomere)
+data_te$maternalChroms <- totalMatChroms(data_te)
+data_te$paternalChroms <- totalPatChroms(data_te)
+data_te$totalChroms <- totalChroms(data_te)
+
+data_sampled <- rbind(data_blastomere[sample(nrow(data_te)),], data_te)
+
+df <- data.frame(mat = data_sampled$maternalChroms, pat = data_sampled$paternalChroms, sample_type = data_sampled$sample_type)
+p <- ggplot(df, aes(x = mat, y = pat)) + stat_binhex() + scale_fill_gradient(name = "count", trans = "log", breaks = 10^(0:6))
+p + facet_grid(. ~ sample_type) + theme_bw()
 
 ####################################################
 
