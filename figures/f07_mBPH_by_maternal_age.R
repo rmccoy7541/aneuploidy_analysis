7a467c707a320120980c1373200828768953b3c6
Rajiv McCoy <rcmccoy@uw.edu>
2015-09-29 14:51:18 -0700

    improve readability

--- figures/f07_mBPH_by_maternal_age.R
+++ figures/f07_mBPH_by_maternal_age.R
@@ -35,9 +35,6 @@ results_all_chroms$X3 <- as.numeric(results_all_chroms$X3)
 results_all_chroms$X5 <- as.numeric(results_all_chroms$X5)
 limits <- aes(ymax = (X1 + X2), ymin = (X1 - X2))
 
-#ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point()  + xlab("Maternal Age") + ylab("Prop. Blastomeres with BPH Aneuploidy") + theme(legend.position="right") + theme_bw() + scale_color_discrete(name = "")
-
-
 results_blastomere <- results_all_chroms
 results_blastomere$X6 <- "Day-3 Blastomere"
 
@@ -71,7 +68,9 @@ results_te$X6 <- "Day-5 TE"
 
 results_all_chroms <- rbind(results_blastomere, results_te)
 
-c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Maternal Age") + ylab("Prop. with Maternal BPH Error") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
+c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], 
+aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Maternal Age") + 
+ylab("Prop. with Maternal BPH Error") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
 
 c <- c + facet_grid(. ~ X6)
 
@@ -89,6 +88,8 @@ meiotic_blastomere <- merge(meiotic_blastomere, maternal_age, "case")
 meiotic_te <- merge(meiotic_te, maternal_age, "case")
 
 
-summary(glm(data = meiotic_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit")))
-summary(glm(data = meiotic_te, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit")))
+summary(glm(data = meiotic_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit")))
+summary(glm(data = meiotic_te, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit")))
 
