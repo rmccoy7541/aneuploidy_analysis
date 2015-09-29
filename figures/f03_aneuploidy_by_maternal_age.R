604bbaed7cdf3a439831f1b90afe2dca6c541266
Rajiv McCoy <rcmccoy@uw.edu>
2015-09-29 14:35:56 -0700

    improve readability

--- figures/f03_aneuploidy_by_maternal_age.R
+++ figures/f03_aneuploidy_by_maternal_age.R
@@ -24,19 +24,25 @@ data_blastomere <- callPloidy(data_blastomere)
 results_all_chroms <- aneuploidyByAge(data_blastomere, "Data")
 
 aneuploid_binom <- aneuploidyByCase(data_blastomere) # get summary of aneuploid / euploid embryos per case
+maternal_age <- cbind(case = data[!duplicated(data$case),]$case, maternal_age = data[!duplicated(data$case),]$maternal_age)
+aneuploid_binom <- merge(aneuploid_binom, maternal_age, "case")
 
-g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age, family = quasibinomial(link = "logit"))
+g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age, 
+family = quasibinomial(link = "logit"))
 
-g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2), 
+family = quasibinomial(link = "logit"))
 
 anova(g0, g1, test="F")
 
-g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test="F")
 
 # Fit model to data
-g3 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + I(maternal_age^4), family = quasibinomial(link = "logit"))
+g3 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3) + I(maternal_age^4), family = quasibinomial(link = "logit"))
 
 anova(g2, g3, test = "F")
 
@@ -56,14 +62,18 @@ results_all_chroms$X3 <- as.numeric(results_all_chroms$X3)
 results_all_chroms$X5 <- as.numeric(results_all_chroms$X5)
 limits <- aes(ymax = (X1 + X2), ymin = (X1 - X2))
 
-ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1, col = factor(X4))) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point()  + xlab("Maternal Age") + ylab("Proportion Aneuploid Biopsies") + theme(legend.position="right") + theme_bw() + scale_color_discrete(name = "")
+ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], 
+aes(x = X3, y = X1, col = factor(X4))) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + 
+xlab("Maternal Age") + ylab("Proportion Aneuploid Biopsies") + theme(legend.position="right") + theme_bw() + 
+scale_color_discrete(name = "")
 
 # Test the effect of egg donor status
 donor <- data.frame(aggregate(data_blastomere$egg_donor ~ data_blastomere$case, FUN = mean))
-names(donor) <- c("i", "donor")
-aneuploid_binom <- merge(aneuploid_binom, donor, "i")
+names(donor) <- c("case", "donor")
+aneuploid_binom <- merge(aneuploid_binom, donor, "case")
 
-g4 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + donor, family = quasibinomial(link = "logit"))
+g4 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3) + donor, family = quasibinomial(link = "logit"))
 
 anova(g2, g4, test = "F")
 
@@ -83,20 +93,21 @@ data_te <- callPloidy(data_te)
 results_all_chroms <- aneuploidyByAge(data_te, "Data")
 
 aneuploid_binom <- aneuploidyByCase(data_te) # get summary of aneuploid / euploid embryos per case
+maternal_age <- cbind(case = data[!duplicated(data$case),]$case, maternal_age = data[!duplicated(data$case),]$maternal_age)
+aneuploid_binom <- merge(aneuploid_binom, maternal_age, "case")
 
-g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age, family = quasibinomial(link = "logit"))
+g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age, 
+family = quasibinomial(link = "logit"))
 
-g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2), 
+family = quasibinomial(link = "logit"))
 
 anova(g0, g1, test="F")
 
-g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test="F")
-
-sum(residuals(g1, type = "pearson")^2)
-deviance(g1)
-pchisq(deviance(g1), df.residual(g1))
 
 # Use model to predict aneuploidy based on age
 prediction_frame <- data.frame("maternal_age" = seq(20,48,1))
@@ -114,25 +125,33 @@ results_all_chroms$X3 <- as.numeric(results_all_chroms$X3)
 results_all_chroms$X5 <- as.numeric(results_all_chroms$X5)
 limits <- aes(ymax = (X1 + X2), ymin = (X1 - X2))
 
-ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1, col = factor(X4))) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point()  + xlab("Maternal Age") + ylab("Proportion Aneuploid Biopsies") + theme(legend.position="right") + theme_bw() + scale_color_discrete(name = "")
+ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], 
+aes(x = X3, y = X1, col = factor(X4))) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + 
+xlab("Maternal Age") + ylab("Proportion Aneuploid Biopsies") + theme(legend.position="right") + theme_bw() + 
+scale_color_discrete(name = "")
 
 # Test the effect of egg donor status
 donor <- data.frame(aggregate(data_te$egg_donor ~ data_te$case, FUN = mean))
-names(donor) <- c("i", "donor")
-aneuploid_binom <- merge(aneuploid_binom, donor, "i")
+names(donor) <- c("case", "donor")
+aneuploid_binom <- merge(aneuploid_binom, donor, "case")
 
-g4 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + donor, family = quasibinomial(link = "logit"))
+g3 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + 
+donor, family = quasibinomial(link = "logit"))
 
-anova(g3, g4, test = "F")
-
+anova(g1, g3, test = "F")
 
 results_te <- results_all_chroms
 results_te$X6 <- "Day-5 TE"
+
+pR2(g1)
 
 ##########################################################
 
 results_all_chroms <- rbind(results_blastomere, results_te)
 
-c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1, col = factor(X4))) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Maternal Age") + ylab("Proportion Aneuploid") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
+c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], 
+aes(x = X3, y = X1, col = factor(X4))) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + 
+xlab("Maternal Age") + ylab("Proportion Aneuploid") + theme(legend.position = "right") + theme_bw() + 
+scale_color_discrete(name = "")
 
 c <- c + facet_grid(. ~ X6)
