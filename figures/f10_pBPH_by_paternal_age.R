e6c74a8ccd105c117a356ca46a8380974d11c09e
Rajiv McCoy <rcmccoy@uw.edu>
2015-09-29 14:55:42 -0700

    improve readability

--- figures/f10_pBPH_by_paternal_age.R
+++ figures/f10_pBPH_by_paternal_age.R
@@ -22,7 +22,6 @@ data_blastomere <- callPaternalMeiotic(data_blastomere)
 
 data_blastomere <- callMitotic(data_blastomere)
 
-
 #################################################################
 
 results_all_chroms <- paternalMeioticByAge(data_blastomere, "Data")
@@ -34,9 +33,6 @@ results_all_chroms$X2 <- as.numeric(results_all_chroms$X2)
 results_all_chroms$X3 <- as.numeric(results_all_chroms$X3)
 results_all_chroms$X5 <- as.numeric(results_all_chroms$X5)
 limits <- aes(ymax = (X1 + X2), ymin = (X1 - X2))
-
-#ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point()  + xlab("Paternal Age") + ylab("Prop. Blastomeres with BPH Aneuploidy") + theme(legend.position="right") + theme_bw() + scale_color_discrete(name = "")
-
 
 results_blastomere <- results_all_chroms
 results_blastomere$X6 <- "Day-3 Blastomere"
@@ -51,11 +47,9 @@ data_te <- callPaternalMeiotic(data_te)
 
 data_te <- callMitotic(data_te)
 
-
 #################################################################
 
 results_all_chroms <- paternalMeioticByAge(data_te, "Data")
-
 
 # Plot the results
 results_all_chroms$X1 <- as.numeric(results_all_chroms$X1)
@@ -71,7 +65,9 @@ results_te$X6 <- "Day-5 TE"
 
 results_all_chroms <- rbind(results_blastomere, results_te)
 
-c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Paternal Age") + ylab("Prop. with Paternal BPH Error") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
+c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + 
+geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Paternal Age") + 
+ylab("Prop. with Paternal BPH Error") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
 
 c <- c + facet_grid(. ~ X6)
 
@@ -84,7 +80,6 @@ paternal_age <- data.frame(aggregate(data$paternal_age ~ data$case, FUN = mean))
 names(paternal_age) <- c("case", "paternal_age")
 meiotic_blastomere <- merge(meiotic_blastomere, paternal_age, "case")
 meiotic_te <- merge(meiotic_te, paternal_age, "case")
-
 
 summary(glm(data = meiotic_blastomere, formula = cbind(euploid, aneuploid) ~ paternal_age, family = quasibinomial(link = "logit")))
 summary(glm(data = meiotic_te, formula = cbind(euploid, aneuploid) ~ paternal_age, family = quasibinomial(link = "logit")))
