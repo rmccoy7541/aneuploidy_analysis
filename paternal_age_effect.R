5ae2a2b4bac7c0342521b00fc8527e0c5b894622
Rajiv McCoy <rcmccoy@uw.edu>
2015-09-29 15:21:45 -0700

    make compatible with functions

--- paternal_age_effect.R
+++ paternal_age_effect.R
@@ -24,21 +24,28 @@ data_blastomere <- callPloidy(data_blastomere)
 
 aneuploid_binom <- aneuploidyByCase(data_blastomere) 
 paternal_age <- data.frame(aggregate(data_filtered$paternal_age ~ data_filtered$case, FUN = mean))
-names(paternal_age) <- c("i", "paternal_age")
-aneuploid_binom <- merge(aneuploid_binom, paternal_age, "i")
+names(paternal_age) <- c("case", "paternal_age")
+aneuploid_binom <- merge(aneuploid_binom, paternal_age, "case")
+maternal_age <- data.frame(aggregate(data_filtered$maternal_age ~ data_filtered$case, FUN = mean))
+names(maternal_age) <- c("case", "maternal_age")
+aneuploid_binom <- merge(aneuploid_binom, maternal_age, "case")
+aneuploid_binom <- data.frame(aneuploid_binom)
 
 ###########################
 ### Partial correlation ###
 ###########################
 
-pcor.test((aneuploid_binom$aneuploid_1/(aneuploid_binom$aneuploid_0+aneuploid_binom$aneuploid_1)), aneuploid_binom$paternal_age, aneuploid_binom$maternal_age, method="spearman")
-pcor.test((aneuploid_binom$aneuploid_1/(aneuploid_binom$aneuploid_0+aneuploid_binom$aneuploid_1)), aneuploid_binom$maternal_age, aneuploid_binom$paternal_age, method="spearman")
+pcor.test((aneuploid_binom$aneuploid / (aneuploid_binom$euploid + aneuploid_binom$aneuploid)), 
+aneuploid_binom$paternal_age, aneuploid_binom$maternal_age, method="spearman")
+
+pcor.test((aneuploid_binom$aneuploid / (aneuploid_binom$euploid + aneuploid_binom$aneuploid)), 
+aneuploid_binom$maternal_age, aneuploid_binom$paternal_age, method="spearman")
 
 ####################
 # caliper = 0.1    #
 ####################
 
-aneuploid_binom$father <- NA
+aneuploid_binom$father <- as.numeric(NA)
 aneuploid_binom[aneuploid_binom$paternal_age < median(aneuploid_binom$paternal_age),]$father <- 0
 aneuploid_binom[aneuploid_binom$paternal_age >= median(aneuploid_binom$paternal_age),]$father <- 1
 
@@ -50,10 +57,11 @@ aneuploid_binom_matched <- cbind(aneuploid_binom[matches$index.treated,], aneupl
 sum(aneuploid_binom_matched[,2]); sum(aneuploid_binom_matched[,3])
 sum(aneuploid_binom_matched[,8]); sum(aneuploid_binom_matched[,9])
 
-sum(aneuploid_binom_matched[,3])/sum(aneuploid_binom_matched[,2]+aneuploid_binom_matched[,3])
+sum(aneuploid_binom_matched[,3]) / sum(aneuploid_binom_matched[,2] + aneuploid_binom_matched[,3])
 sum(aneuploid_binom_matched[,9])/sum(aneuploid_binom_matched[,8]+aneuploid_binom_matched[,9])
 
-fisher.test(rbind(cbind(sum(aneuploid_binom_matched[,2]), sum(aneuploid_binom_matched[,3])), cbind(sum(aneuploid_binom_matched[,8]), sum(aneuploid_binom_matched[,9]))))
+fisher.test(rbind(cbind(sum(aneuploid_binom_matched[,2]), sum(aneuploid_binom_matched[,3])), 
+cbind(sum(aneuploid_binom_matched[,8]), sum(aneuploid_binom_matched[,9]))))
 
 ####################
 # caliper = 0.01    #
@@ -80,31 +88,31 @@ fisher.test(rbind(cbind(sum(aneuploid_binom_matched[,2]), sum(aneuploid_binom_ma
 ### GLM F-Test   	    ###
 ###########################
 
-g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age, family = quasibinomial(link = "logit"))
-g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age, family = quasibinomial(link = "logit"))
+g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
 
 anova(g0, g1, test = "F")
 
-g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test = "F")
 
-g3 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + I(maternal_age^4), family = quasibinomial(link = "logit"))
+g3 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + I(maternal_age^4), family = quasibinomial(link = "logit"))
 
 anova(g2, g3, test = "F")
 
-g4 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + paternal_age, family = quasibinomial(link = "logit"))
+g4 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + paternal_age, family = quasibinomial(link = "logit"))
 
 anova(g2, g4, test = "F")
 
 ###########################
 
 donor <- data.frame(aggregate(data_filtered$egg_donor ~ data_filtered$case, FUN = mean))
-names(donor) <- c("i", "donor")
-aneuploid_binom <- merge(aneuploid_binom, donor, "i")
+names(donor) <- c("case", "donor")
+aneuploid_binom <- merge(aneuploid_binom, donor, "case")
 
-f1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ paternal_age, family = quasibinomial(link = "logit"))
-f2 <- glm(data = aneuploid_binom[aneuploid_binom$donor == 1,], formula = cbind(aneuploid_1, aneuploid_0) ~ paternal_age, family = quasibinomial(link = "logit"))
+f1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ paternal_age, family = quasibinomial(link = "logit"))
+f2 <- glm(data = aneuploid_binom[aneuploid_binom$donor == 1,], formula = cbind(aneuploid, euploid) ~ paternal_age, family = quasibinomial(link = "logit"))
 
 #########################################
 
@@ -116,15 +124,19 @@ data_te <- callPloidy(data_te)
 
 aneuploid_binom <- aneuploidyByCase(data_te) 
 paternal_age <- data.frame(aggregate(data_filtered$paternal_age ~ data_filtered$case, FUN = mean))
-names(paternal_age) <- c("i", "paternal_age")
-aneuploid_binom <- merge(aneuploid_binom, paternal_age, "i")
+names(paternal_age) <- c("case", "paternal_age")
+aneuploid_binom <- merge(aneuploid_binom, paternal_age, "case")
+maternal_age <- data.frame(aggregate(data_filtered$maternal_age ~ data_filtered$case, FUN = mean))
+names(maternal_age) <- c("case", "maternal_age")
+aneuploid_binom <- merge(aneuploid_binom, maternal_age, "case")
+aneuploid_binom <- data.frame(aneuploid_binom)
 
 ###########################
 ### Partial correlation ###
 ###########################
 
-pcor.test((aneuploid_binom$aneuploid_1/(aneuploid_binom$aneuploid_0+aneuploid_binom$aneuploid_1)), aneuploid_binom$paternal_age, aneuploid_binom$maternal_age, method="spearman")
-pcor.test((aneuploid_binom$aneuploid_1/(aneuploid_binom$aneuploid_0+aneuploid_binom$aneuploid_1)), aneuploid_binom$maternal_age, aneuploid_binom$paternal_age, method="spearman")
+pcor.test((aneuploid_binom$aneuploid/(aneuploid_binom$euploid+aneuploid_binom$aneuploid)), aneuploid_binom$paternal_age, aneuploid_binom$maternal_age, method="spearman")
+pcor.test((aneuploid_binom$aneuploid/(aneuploid_binom$euploid+aneuploid_binom$aneuploid)), aneuploid_binom$maternal_age, aneuploid_binom$paternal_age, method="spearman")
 
 ####################
 # caliper = 0.1    #
@@ -151,24 +163,24 @@ fisher.test(rbind(cbind(sum(aneuploid_binom_matched[,2]), sum(aneuploid_binom_ma
 ### GLM F-Test   	    ###
 ###########################
 
-g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age, family = quasibinomial(link = "logit"))
-g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g0 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age, family = quasibinomial(link = "logit"))
+g1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
 
 anova(g0, g1, test = "F")
 
-g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g2 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test = "F")
 
-g3 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ maternal_age + I(maternal_age^2) + paternal_age, family = quasibinomial(link = "logit"))
+g3 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ maternal_age + I(maternal_age^2) + paternal_age, family = quasibinomial(link = "logit"))
 
 anova(g1, g3, test = "F")
 
 ###########################
 
 donor <- data.frame(aggregate(data_filtered$egg_donor ~ data_filtered$case, FUN = mean))
-names(donor) <- c("i", "donor")
-aneuploid_binom <- merge(aneuploid_binom, donor, "i")
+names(donor) <- c("case", "donor")
+aneuploid_binom <- merge(aneuploid_binom, donor, "case")
 
-f1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid_1, aneuploid_0) ~ paternal_age, family = quasibinomial(link = "logit"))
-f2 <- glm(data = aneuploid_binom[aneuploid_binom$donor == 1,], formula = cbind(aneuploid_1, aneuploid_0) ~ paternal_age, family = quasibinomial(link = "logit"))
+f1 <- glm(data = aneuploid_binom, formula = cbind(aneuploid, euploid) ~ paternal_age, family = quasibinomial(link = "logit"))
+f2 <- glm(data = aneuploid_binom[aneuploid_binom$donor == 1,], formula = cbind(aneuploid, euploid) ~ paternal_age, family = quasibinomial(link = "logit"))
