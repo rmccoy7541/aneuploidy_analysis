df1542bd6b2a0cf217d2a1f38b22fdba483a1766
Rajiv McCoy <rcmccoy@uw.edu>
2015-09-29 14:56:37 -0700

    improve readability

--- figures/f11_mitotic_by_maternal_age.R
+++ figures/f11_mitotic_by_maternal_age.R
@@ -22,11 +22,9 @@ data_blastomere <- callPaternalMeiotic(data_blastomere)
 
 data_blastomere <- callMitotic(data_blastomere)
 
-
 #################################################################
 
 results_all_chroms <- mitoticByAge(data_blastomere, "Data")
-
 
 # Plot the results
 results_all_chroms$X1 <- as.numeric(results_all_chroms$X1)
@@ -34,9 +32,6 @@ results_all_chroms$X2 <- as.numeric(results_all_chroms$X2)
 results_all_chroms$X3 <- as.numeric(results_all_chroms$X3)
 results_all_chroms$X5 <- as.numeric(results_all_chroms$X5)
 limits <- aes(ymax = (X1 + X2), ymin = (X1 - X2))
-
-#ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point()  + xlab("Maternal Age") + ylab("Prop. Blastomeres with BPH Aneuploidy") + theme(legend.position="right") + theme_bw() + scale_color_discrete(name = "")
-
 
 results_blastomere <- results_all_chroms
 results_blastomere$X6 <- "Day-3 Blastomere"
@@ -51,11 +46,9 @@ data_te <- callPaternalMeiotic(data_te)
 
 data_te <- callMitotic(data_te)
 
-
 #################################################################
 
 results_all_chroms <- mitoticByAge(data_te, "Data")
-
 
 # Plot the results
 results_all_chroms$X1 <- as.numeric(results_all_chroms$X1)
@@ -71,23 +64,23 @@ results_te$X6 <- "Day-5 TE"
 
 results_all_chroms <- rbind(results_blastomere, results_te)
 
-c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Maternal Age") + ylab("Prop. with Mitotic Error") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
+c <- ggplot(data = results_all_chroms[results_all_chroms$X5 > 9 | is.na(results_all_chroms$X5),], 
+aes(x = X3, y = X1)) + geom_errorbar(limits, width = 0.5) + geom_line() + geom_point() + xlab("Maternal Age") + 
+ylab("Prop. with Mitotic Error") + theme(legend.position = "right") + theme_bw() + scale_color_discrete(name = "")
 
 c <- c + facet_grid(. ~ X6)
 
-
 ##########################################################
-
 
 mitotic_blastomere <- mitoticByCase(data_blastomere)
 mitotic_te <- mitoticByCase(data_te)
-
 
 maternal_age <- data.frame(aggregate(data$maternal_age ~ data$case, FUN = mean))
 names(maternal_age) <- c("case", "maternal_age")
 mitotic_blastomere <- merge(mitotic_blastomere, maternal_age, "case")
 mitotic_te <- merge(mitotic_te, maternal_age, "case")
 
-
-summary(glm(data = mitotic_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit")))
-summary(glm(data = mitotic_te, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit")))
+summary(glm(data = mitotic_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit")))
+summary(glm(data = mitotic_te, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit")))
