f38dfe7ad62ce264e069a8e30fc89e2ffc1be22e
Rajiv McCoy <rcmccoy@uw.edu>
2015-09-29 15:07:38 -0700

    improve readability

--- figures/f16_referral_reasons.R
+++ figures/f16_referral_reasons.R
@@ -41,7 +41,7 @@ te_aneuploidy_counts <- aneuploidyByCase(data_te)
 maternal_age <- data.frame(cbind(data[!duplicated(data$case),]$case, data[!duplicated(data$case),]$maternal_age))
 names(maternal_age) <- c("case", "maternal_age")
 
-referral_reasons <- read.table("~/Desktop/aneuploidy-analysis/data/referral_reasons.csv", sep = ",", header = T)
+referral_reasons <- read.table("~/Desktop/aneuploidy_analysis-master/data/referral_reasons.csv", sep = ",", header = T)
 
 referral_reasons <- merge(referral_reasons, maternal_age, "case")
 
@@ -49,31 +49,41 @@ referral_reasons <- merge(referral_reasons, maternal_age, "case")
 
 referral_reasons_blastomere <- merge(referral_reasons, blastomere_aneuploidy_counts, "case")
 
-g1 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit"))
-g2 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g1 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit"))
+g2 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2), family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test = "F")
 
-g3 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g3 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g2, g3, test = "F")
 
-aneuploidy_blastomere <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
+aneuploidy_blastomere <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2) + repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + 
+translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
 
 #################################################################
 
 referral_reasons_te <- merge(referral_reasons, te_aneuploidy_counts, "case")
 
-g1 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit"))
-g2 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g1 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit"))
+g2 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2), 
+family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test = "F")
 
-g3 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g3 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g2, g3, test = "F")
 
-aneuploidy_te <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
+aneuploidy_te <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2) + repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + 
+translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
 
 #################################################################
 # Test mitotic-origin aneuploidies separately                   #
@@ -87,13 +97,17 @@ te_mitotic_counts <- mitoticByCase(data_te)
 
 referral_reasons_blastomere <- merge(referral_reasons, blastomere_mitotic_counts, "case")
 
-mitotic_blastomere <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
+mitotic_blastomere <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ 
+repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + translocation + 
+previous_aneuploidy, family = quasibinomial(link = "logit"))
 
 #################################################################
 
 referral_reasons_te <- merge(referral_reasons, te_mitotic_counts, "case")
 
-mitotic_te <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
+mitotic_te <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ repeat_pregnancy_loss + 
+previous_ivf_failure + male_factor + unexplained_infertility + translocation + previous_aneuploidy, 
+family = quasibinomial(link = "logit"))
 
 #################################################################
 # Test meiotic-origin aneuploidies separately                   #
@@ -107,39 +121,51 @@ te_meiotic_counts <- meioticByCase(data_te)
 
 referral_reasons_blastomere <- merge(referral_reasons, blastomere_meiotic_counts, "case")
 
-g1 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit"))
-g2 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g1 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit"))
+g2 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2), family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test = "F")
 
-g3 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g3 <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g2, g3, test = "F")
 
-meiotic_blastomere <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
+meiotic_blastomere <- glm(data = referral_reasons_blastomere, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2) + repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + 
+translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
 
 #################################################################
 
 referral_reasons_te <- merge(referral_reasons, te_meiotic_counts, "case")
 
-g1 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age, family = quasibinomial(link = "logit"))
-g2 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2), family = quasibinomial(link = "logit"))
+g1 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age, 
+family = quasibinomial(link = "logit"))
+g2 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2), family = quasibinomial(link = "logit"))
 
 anova(g1, g2, test = "F")
 
-g3 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3), family = quasibinomial(link = "logit"))
+g3 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3), family = quasibinomial(link = "logit"))
 
 anova(g2, g3, test = "F")
 
-g4 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + I(maternal_age^4), family = quasibinomial(link = "logit"))
+g4 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3) + I(maternal_age^4), family = quasibinomial(link = "logit"))
 
 anova(g3, g4, test = "F")
 
-g5 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + I(maternal_age^4) + I(maternal_age^5), family = quasibinomial(link = "logit"))
+g5 <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + 
+I(maternal_age^3) + I(maternal_age^4) + I(maternal_age^5), family = quasibinomial(link = "logit"))
 
 anova(g4, g5, test = "F")
 
-meiotic_te <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + I(maternal_age^2) + I(maternal_age^3) + I(maternal_age^4) + repeat_pregnancy_loss + previous_ivf_failure + male_factor + unexplained_infertility + translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
+meiotic_te <- glm(data = referral_reasons_te, formula = cbind(euploid, aneuploid) ~ maternal_age + 
+I(maternal_age^2) + I(maternal_age^3) + I(maternal_age^4) + repeat_pregnancy_loss + previous_ivf_failure + 
+male_factor + unexplained_infertility + translocation + previous_aneuploidy, family = quasibinomial(link = "logit"))
 
 int_aneuploidy_blastomere <- confint(aneuploidy_blastomere)
 int_aneuploidy_te <- confint(aneuploidy_te)
@@ -169,7 +195,8 @@ results_frame$terms <- as.factor(results_frame$terms)
 results_frame$beta <- as.numeric(as.character(results_frame$beta))
 results_frame$se <- as.numeric(as.character(results_frame$se))
 results_frame$p <- as.numeric(as.character(results_frame$p))
-setattr(results_frame$terms, "levels", c("Male factor infertility", "Prev. aneuploid conception", "Previous IVF failure", "Recurr. pregnancy loss", "Translocation carrier", "Unexplained infertility"))
+setattr(results_frame$terms, "levels", c("Male factor infertility", "Prev. aneuploid conception", 
+"Previous IVF failure", "Recurr. pregnancy loss", "Translocation carrier", "Unexplained infertility"))
 
 
 results_frame$star <- ""
@@ -178,22 +205,34 @@ results_frame[p < 0.01]$star <- "**"
 results_frame[p < 0.001]$star <- "***"
 
 limits <- aes(xmax = beta + se, xmin = beta - se)
-p <- ggplot(data = results_frame, aes(x = beta, y = factor(terms), col = factor(terms))) + theme_bw() + geom_point() + geom_errorbarh(limits, height = 0)
-p + facet_grid(aneuploidy ~ dev_stage) + ylab('Referral Reason') + xlab('Coefficient') + geom_vline(xintercept = 0, linetype = "dotted") + theme(legend.position="none") + geom_text(aes(x = beta, label = star, vjust = 0.1))
+p <- ggplot(data = results_frame, aes(x = beta, y = factor(terms), col = factor(terms))) + theme_bw() + geom_point() + 
+geom_errorbarh(limits, height = 0)
+p + facet_grid(aneuploidy ~ dev_stage) + ylab('Referral Reason') + xlab('Coefficient') + 
+geom_vline(xintercept = 0, linetype = "dotted") + theme(legend.position="none") + 
+geom_text(aes(x = beta, label = star, vjust = 0.1))
 
 results_frame$beta <- (-1) * results_frame$beta
 results_frame$or <- exp(1) ^ (results_frame$beta)
 
-results_frame <- cbind(results_frame, rbind(int_aneuploidy_blastomere[4:nrow(int_aneuploidy_blastomere),], int_aneuploidy_te[4:nrow(int_aneuploidy_te),], int_meiotic_blastomere[4:nrow(int_meiotic_blastomere),], int_meiotic_te[6:nrow(int_meiotic_te),], int_mitotic_blastomere[2:nrow(int_mitotic_blastomere),], int_mitotic_te[2:nrow(int_mitotic_te),]))
+results_frame <- cbind(results_frame, rbind(int_aneuploidy_blastomere[4:nrow(int_aneuploidy_blastomere),], 
+int_aneuploidy_te[4:nrow(int_aneuploidy_te),], int_meiotic_blastomere[4:nrow(int_meiotic_blastomere),], 
+int_meiotic_te[6:nrow(int_meiotic_te),], int_mitotic_blastomere[2:nrow(int_mitotic_blastomere),], 
+int_mitotic_te[2:nrow(int_mitotic_te),]))
 setnames(results_frame, c("terms", "beta", "se", "p", "aneuploidy", "dev_stage", "star", "or", "lowerCI", "upperCI"))
 
 results_frame$ul <- exp(1) ^ (results_frame$lowerCI * (-1))
 results_frame$ll <- exp(1) ^ (results_frame$upperCI * (-1))
 
 limits <- aes(xmax = ul, xmin = ll)
-p <- ggplot(data = results_frame, aes(x = or, y = factor(terms), col = factor(terms))) + theme_bw() + geom_point() + geom_errorbarh(limits, height = 0)
-p + facet_grid(aneuploidy ~ dev_stage) + ylab('Referral Reason') + xlab('Odds Ratio, 95% CI') + geom_vline(xintercept = 1, linetype = "dotted") + theme(legend.position="none") + geom_text(aes(x = or, label = star, vjust = 0.1))
+p <- ggplot(data = results_frame, aes(x = or, y = factor(terms), col = factor(terms))) + theme_bw() + geom_point() + 
+geom_errorbarh(limits, height = 0)
+p + facet_grid(aneuploidy ~ dev_stage) + ylab('Referral Reason') + xlab('Odds Ratio, 95% CI') + 
+geom_vline(xintercept = 1, linetype = "dotted") + theme(legend.position="none") + 
+geom_text(aes(x = or, label = star, vjust = 0.1))
 
-
-q <- ggplot(data = results_frame[terms == "Translocation carrier" | terms == "Previous IVF failure" | terms == "Recurr. pregnancy loss"], aes(x = or, y = factor(terms), col = factor(terms))) + theme_bw() + geom_point() + geom_errorbarh(limits, height = 0)
-q + facet_grid(aneuploidy ~ dev_stage) + ylab('Referral Reason') + xlab('Odds Ratio, 95% CI') + geom_vline(xintercept = 1, linetype = "dotted") + theme(legend.position="none") + geom_text(aes(x = or, label = star, vjust = 0.1))
+q <- ggplot(data = results_frame[terms == "Translocation carrier" | terms == "Previous IVF failure" | 
+terms == "Recurr. pregnancy loss"], aes(x = or, y = factor(terms), col = factor(terms))) + theme_bw() + 
+geom_point() + geom_errorbarh(limits, height = 0)
+q + facet_grid(aneuploidy ~ dev_stage) + ylab('Referral Reason') + xlab('Odds Ratio, 95% CI') + 
+geom_vline(xintercept = 1, linetype = "dotted") + theme(legend.position="none") + 
+geom_text(aes(x = or, label = star, vjust = 0.1))
